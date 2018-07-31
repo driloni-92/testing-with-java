7ecf9a175b35e501c42cce3c4c983d09bdfb2ae5
driloni-92 <driloni92@hotmail.com>
2018-07-31 15:25:52 +0100

    # Completed Fibonnaci series exercise

--- step-2-tdd-fibonacci-junit/src/Fibonacci.java
+++ step-2-tdd-fibonacci-junit/src/Fibonacci.java
@@ -1,2 +1,13 @@
 public class Fibonacci {
+    public int calculate(int index) {
+
+        if (index < 0) {
+            throw new IllegalArgumentException("Fibonnaci series does not have negative numbers");
+        }
+
+        if (index == 0 || index ==1) {
+            return 1;
+        }
+        return calculate(index - 1) + calculate(index - 2);
+    }
 }
